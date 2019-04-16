334964eb3c2087e6f29db91244000ccf6275430a
Luke Park <luke@mpb.com>
2019-04-16 13:04:07 +0100

    Serve the home page

--- project/bswing/views.py
+++ project/bswing/views.py
@@ -1,6 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.template import loader
 
-def home():
-    pass
+
+def home(request):
+    template = loader.get_template('index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
